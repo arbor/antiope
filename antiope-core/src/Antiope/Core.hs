d1781a66ae8bd15277675ea44c56f83b024283e6
alexeyraga <alexey.raga@gmail.com>
2018-04-23 09:42:46 +1000

    Expose ToText/FromText

---
+++ antiope-core/src/Antiope/Core.hs
@@ -0,0 +1,9 @@
+module Antiope.Core
+( module Network.AWS.Data.Text
+
+)
+where
+
+import           Network.AWS.Data.Text (FromText (..), Text, ToText (..),
+                                        fromText, toText)
+

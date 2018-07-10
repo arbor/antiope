309bf135dc9666bc50ab32637c4734bb4acb012c
David Turnbull <david@broodax.net>
2018-07-10 10:51:43 +1000

    MAYH-9062 non-leaking sqs message acknowledgement

--- antiope-sqs/src/Antiope/SQS.hs
+++ antiope-sqs/src/Antiope/SQS.hs
@@ -5,6 +5,7 @@ module Antiope.SQS
 , FromText(..), fromText
 , ToText(..)
 , QueueUrl(..)
+, SQSError(..)
 , readQueue
 , drainQueue
 , ackMessage
@@ -12,21 +13,24 @@ module Antiope.SQS
 , module Network.AWS.SQS
 ) where
 
-import           Control.Lens          ((&), (?~), (^.))
-import           Control.Monad         (forM_, join, void)
-import           Control.Monad.Loops   (unfoldWhileM)
-import           Network.AWS           (MonadAWS, send)
-import           Network.AWS.SQS
+import Control.Lens         (each, (&), (.~), (?~), (^.), (^..))
+import Control.Monad        (forM_, join, void, when)
+import Control.Monad.Except (ExceptT (..), throwError)
+import Control.Monad.Loops  (unfoldWhileM)
+import Data.Maybe           (catMaybes)
+import Network.AWS          (MonadAWS, send)
+import Network.AWS.SQS
 
-import           Data.String           (IsString)
-import           Data.Text             (Text)
-import           Network.AWS.Data.Text (FromText (..), ToText (..), fromText,
-                                        toText)
+import Data.String           (IsString)
+import Data.Text             (Text, pack)
+import Network.AWS.Data.Text (FromText (..), ToText (..), fromText, toText)
+
+data SQSError = DeleteMessageBatchError
 
 newtype QueueUrl = QueueUrl { unQueueUrl :: Text } deriving (Show, Eq, IsString, FromText, ToText)
 
 -- | Reads the specified SQS queue once returning a bath of messages
-readQueue :: (MonadAWS m) => QueueUrl -> m [Message]
+readQueue :: MonadAWS m => QueueUrl -> m [Message]
 readQueue (QueueUrl queueUrl) = do
   -- max wait time allowed by aws is 20sec, max number messages to recieve is 10
   resp <- send $ receiveMessage queueUrl & rmWaitTimeSeconds ?~ 10 & rmMaxNumberOfMessages ?~ 10
@@ -37,12 +41,17 @@ drainQueue :: MonadAWS m => QueueUrl -> m [Message]
 drainQueue queueUrl =
   join <$> unfoldWhileM (not . null) (readQueue queueUrl)
 
-ackMessage :: MonadAWS m => QueueUrl -> Message -> m ()
-ackMessage (QueueUrl queueUrl) msg =
-  case msg ^. mReceiptHandle of
-    Nothing -> pure ()
-    Just rc -> void . send $ deleteMessage queueUrl rc
+ackMessage :: MonadAWS m => QueueUrl -> Message -> ExceptT SQSError m ()
+ackMessage q msg = ackMessages q [msg]
 
-ackMessages :: MonadAWS m => QueueUrl -> [Message] -> m ()
-ackMessages queueUrl messages =
-  forM_ messages (ackMessage queueUrl)
+ackMessages :: MonadAWS m => QueueUrl -> [Message] -> ExceptT SQSError m ()
+ackMessages (QueueUrl queueUrl) msgs = do
+  let receipts = catMaybes $ msgs ^.. each . mReceiptHandle
+  -- each dmbr needs an ID. just use the list index.
+  let dmbres = (\(r, i) -> deleteMessageBatchRequestEntry (pack (show i)) r) <$> zip receipts ([0..] :: [Int])
+  resp <- send $ deleteMessageBatch queueUrl & dmbEntries .~ dmbres
+  -- only acceptable if no errors.
+  when (resp ^. dmbrsResponseStatus == 200) $
+      case resp ^. dmbrsFailed of
+        [] -> return ()
+        _  -> throwError DeleteMessageBatchError

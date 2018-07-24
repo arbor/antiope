3bbef34a8bb0e91370b7adb54703ad78a8d25423
Jian Wan <jwan@arbor.net>
2018-07-24 12:53:59 +1000

    fix comments

--- antiope-sns/src/Antiope/SNS.hs
+++ antiope-sns/src/Antiope/SNS.hs
@@ -1,21 +1,31 @@
 module Antiope.SNS
 ( MonadAWS
-, publishMsg
-, subscribeMsg
+, MessageId (..)
+, Protocol (..)
+, SubscriptionArn (..)
+, publishMessage
+, subscribeMessage
 , module Network.AWS.SNS
 ) where
 
 import Control.Lens
-import Data.Text       (Text)
-import Network.AWS     (MonadAWS, send)
+import Data.Text             (Text)
+import Network.AWS           (MonadAWS, send)
+import Network.AWS.Data.Text (FromText (..), ToText (..))
 import Network.AWS.SNS
 
-publishMsg :: MonadAWS m => Text -> Text -> m (Maybe Text)
-publishMsg topicArn message = do
-  resp <- send $ publish message & pTopicARN ?~ topicArn
-  return $ resp ^. prsMessageId
+newtype MessageId = MessageId Text deriving (Show, Eq, Ord, ToText, FromText)
+newtype Protocol = Protocol Text deriving (Show, Eq, Ord, ToText, FromText)
+newtype SubscriptionArn = SubscriptionArn Text deriving (Show, Eq, Ord, ToText, FromText)
 
-subscribeMsg :: MonadAWS m => Text -> Text -> Text -> m (Maybe Text)
-subscribeMsg topicArn protocol ep = do
-  resp <- send $ subscribe topicArn protocol & subEndpoint ?~ ep
-  return $ resp ^. srsSubscriptionARN
+publishMessage :: MonadAWS m => Topic -> Text -> m (Maybe MessageId)
+publishMessage topicArn message = do
+  resp <- send $ publish message & pTopicARN <>~ (topicArn ^. tTopicARN)
+  return $ MessageId <$> resp ^. prsMessageId
+
+subscribeMessage :: MonadAWS m => Topic -> Protocol -> Endpoint -> m (Maybe SubscriptionArn)
+subscribeMessage topicArn (Protocol p) ep = case topicArn ^. tTopicARN of
+  Just t -> do
+    resp <- send $ subscribe t p & subEndpoint <>~ (ep ^. eEndpointARN)
+    return $ SubscriptionArn <$> resp ^. srsSubscriptionARN
+  Nothing -> return Nothing
--- antiope-sqs/src/Antiope/SQS.hs
+++ antiope-sqs/src/Antiope/SQS.hs
@@ -8,7 +8,7 @@ module Antiope.SQS
 , drainQueue
 , ackMessage
 , ackMessages
-, msgInBody
+, messageInBody
 , s3Location
 , s3Location'
 , module Network.AWS.SQS
@@ -61,15 +61,16 @@ ackMessages (QueueUrl queueUrl) msgs = do
         _  -> return $ Left DeleteMessageBatchError
     else return $ Left DeleteMessageBatchError
 
-msgInBody :: Text -> Maybe Text
-msgInBody msg = msg ^? key "Message" . _String
+-- Extract the "Message" content in the body if have
+messageInBody :: Text -> Maybe Text
+messageInBody body = body ^? key "Message" . _String
 
 s3Location :: Message -> Maybe S3Uri
 s3Location msg = join $ s3Location' <$> msg ^. mBody
 
 s3Location' :: Text -> Maybe S3Uri
 s3Location' msg = do
-  s3m <- msgInBody msg ^? _Just . key "Records" . nth 0 . key "s3"
+  s3m <- messageInBody msg ^? _Just . key "Records" . nth 0 . key "s3"
   b   <- s3m ^? key "bucket" . key "name" . _String
   k   <- s3m ^? key "object" . key "key" . _String
   pure $ S3Uri (BucketName b) (ObjectKey $ uriDecode k)

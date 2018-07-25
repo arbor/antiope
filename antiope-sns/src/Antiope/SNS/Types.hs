dcd19c64173fead8f9c9fbcea36ab742bc2badaf
Jian Wan <jwan@arbor.net>
2018-07-25 11:20:45 +1000

    remove SNSError

--- antiope-sns/src/Antiope/SNS/Types.hs
+++ antiope-sns/src/Antiope/SNS/Types.hs
@@ -1,13 +1,7 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module Antiope.SNS.Types where
 
 import Data.Text             (Text)
-import GHC.Generics
 import Network.AWS.Data.Text (FromText (..), ToText (..))
-
-data SNSError = PublishMessageError
-  deriving (Eq, Show, Generic)
 
 newtype MessageId = MessageId Text deriving (Show, Eq, Ord, ToText, FromText)
 newtype Protocol = Protocol Text deriving (Show, Eq, Ord, ToText, FromText)

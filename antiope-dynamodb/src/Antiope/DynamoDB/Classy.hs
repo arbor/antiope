d67686ec66c0a2f71f38c5304418cde8def5a110
John Ky <newhoggy@gmail.com>
2018-08-01 16:33:08 +1000

    Classy API

---
+++ antiope-dynamodb/src/Antiope/DynamoDB/Classy.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Antiope.DynamoDB.Classy
+( FromText(..), fromText
+, ToText(..)
+, TableName(..)
+, dynamoPutItem
+, dynamoQuery
+, module Network.AWS.DynamoDB
+) where
+
+import Antiope.DynamoDB.Types (TableName (TableName))
+import Control.Lens
+import Data.HashMap.Strict    (HashMap)
+import Data.Text              (Text)
+import Network.AWS            (MonadAWS)
+import Network.AWS.Data.Text  (FromText (..), ToText (..), fromText, toText)
+import Network.AWS.DynamoDB
+
+import qualified Network.AWS as AWS
+
+dynamoPutItem :: MonadAWS m
+  => TableName
+  -> HashMap Text AttributeValue
+  -> m PutItemResponse
+dynamoPutItem table item = AWS.send $ putItem (table & toText) & piItem .~ item
+
+dynamoQuery :: MonadAWS m
+  => TableName
+  -> (Query
+  -> Query)
+  -> m QueryResponse
+dynamoQuery table f = AWS.send $ f $ query (table & toText)

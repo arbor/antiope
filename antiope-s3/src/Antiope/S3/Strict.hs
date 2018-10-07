be61b161026032827e3cc48fe0f4020982c9f53a
John Ky <newhoggy@gmail.com>
2018-10-08 10:07:43 +1100

    Use strict fmap to force evaluate

--- antiope-s3/src/Antiope/S3/Strict.hs
+++ antiope-s3/src/Antiope/S3/Strict.hs
@@ -8,6 +8,7 @@ module Antiope.S3.Strict
 
 import Antiope.Core                 ()
 import Antiope.S3.Types             (S3Uri)
+import Control.Monad                ((<$!>))
 import Control.Monad.Trans.AWS      hiding (send)
 import Control.Monad.Trans.Resource
 import Network.AWS                  (MonadAWS)
@@ -21,15 +22,15 @@ unsafeDownload :: (MonadAWS m, MonadUnliftIO m)
   => AWS.BucketName
   -> AWS.ObjectKey
   -> m BS.ByteString
-unsafeDownload bucketName objectKey = runResourceT $ LBS.toStrict <$> LBS.unsafeDownload bucketName objectKey
+unsafeDownload bucketName objectKey = runResourceT $ LBS.toStrict <$!> LBS.unsafeDownload bucketName objectKey
 
 download :: (MonadAWS m, MonadUnliftIO m)
   => AWS.BucketName
   -> AWS.ObjectKey
   -> m (Maybe BS.ByteString)
-download bucketName objectKey = runResourceT $ fmap LBS.toStrict <$> LBS.download bucketName objectKey
+download bucketName objectKey = runResourceT $  (LBS.toStrict <$!>) <$!> LBS.download bucketName objectKey
 
 downloadFromS3Uri :: (MonadAWS m, MonadUnliftIO m)
   => S3Uri
   -> m (Maybe BS.ByteString)
-downloadFromS3Uri s3Uri = runResourceT $ fmap LBS.toStrict <$> LBS.downloadFromS3Uri s3Uri
+downloadFromS3Uri s3Uri = runResourceT $ (LBS.toStrict <$!>) <$!> LBS.downloadFromS3Uri s3Uri

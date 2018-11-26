c77777132827e96e387a7f03eaba061469f23311
John Ky <newhoggy@gmail.com>
2018-11-27 09:45:12 +1100

    Add ability to download with GetObject directly

--- antiope-s3/src/Antiope/S3/Lazy.hs
+++ antiope-s3/src/Antiope/S3/Lazy.hs
@@ -2,40 +2,46 @@
 
 module Antiope.S3.Lazy
   ( unsafeDownload
+  , unsafeDownloadRequest
   , download
+  , downloadRequest
   , downloadFromS3Uri
   ) where
 
+import Antiope.Core.Error
 import Antiope.S3.GetObject
 import Antiope.S3.Types             (S3Uri (S3Uri))
 import Control.Lens
-import Control.Monad.Catch          (catch)
 import Control.Monad.Trans.Resource
-import Network.AWS                  (Error (..), MonadAWS, ServiceError (..))
-import Network.HTTP.Types.Status    (Status (..))
+import Network.AWS                  (MonadAWS)
 
 import qualified Data.ByteString.Lazy as LBS
+import qualified Network.AWS          as AWS
 import qualified Network.AWS.S3       as AWS
+
+unsafeDownloadRequest :: (MonadAWS m, MonadResource m)
+  => AWS.GetObject
+  -> m LBS.ByteString
+unsafeDownloadRequest req = do
+  resp <- AWS.send req
+  lazyByteString (resp ^. AWS.gorsBody)
 
 unsafeDownload :: (MonadAWS m, MonadResource m)
   => AWS.BucketName
   -> AWS.ObjectKey
   -> m LBS.ByteString
-unsafeDownload bucketName objectKey = do
-  resp <- getObject bucketName objectKey
-  lazyByteString (resp ^. AWS.gorsBody)
+unsafeDownload bucketName objectKey = unsafeDownloadRequest (AWS.getObject bucketName objectKey)
+
+downloadRequest :: (MonadAWS m, MonadResource m)
+  => AWS.GetObject
+  -> m (Maybe LBS.ByteString)
+downloadRequest req = handle404ToNone (unsafeDownloadRequest req)
 
 download :: (MonadAWS m, MonadResource m)
   => AWS.BucketName
   -> AWS.ObjectKey
   -> m (Maybe LBS.ByteString)
-download bucketName objectKey = do
-  ebs <- (Right <$> unsafeDownload bucketName objectKey) `catch` \(err :: Error) -> case err of
-    (ServiceError (ServiceError' _ (Status 404 _) _ _ _ _)) -> return (Left LBS.empty)
-    _                                                       -> throwM err
-  case ebs of
-    Right bs -> return (Just bs)
-    Left _   -> return Nothing
+download bucketName objectKey = downloadRequest (AWS.getObject bucketName objectKey)
 
 downloadFromS3Uri :: (MonadAWS m, MonadResource m)
   => S3Uri

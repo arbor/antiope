ba47870dd98dbf6d0c81235805a1f0e622a9c9db
John Ky <newhoggy@gmail.com>
2019-09-19 11:11:03 +1000

    New antiope-shell package

---
+++ antiope-shell/test/Antiope/Shell/S3Spec.hs
@@ -0,0 +1,14 @@
+module Antiope.Shell.S3Spec where
+
+import HaskellWorks.Hspec.Hedgehog
+import Hedgehog
+import Test.Hspec
+
+{-# ANN module ("HLint: Ignore Redundant do" :: String) #-}
+
+spec :: Spec
+spec = describe "Antiope.S3.S3Spec" $ do
+  it "Test stub" $ requireTest $ do
+    True === True
+
+
